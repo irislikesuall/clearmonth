822882d273bad2a1558cc2cbd77e6b25a78124e7
irislikesuall <irislikesuall@gmail.com>
2025-12-06 17:15:02 +0100

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -5,39 +5,3 @@ export default {
   },
 }
 ```
-
-5.  点击右上角的 **"Commit changes..."**，然后点击绿色的 **"Commit changes"** 按钮保存。
-
----
-
-### 第二步：等待 Vercel 自动修复
-
-你**不需要**去 Vercel 做任何操作。
-
-当你点击“Commit changes”的一瞬间，Vercel 就已经收到了通知：“嘿，有新文件了，我重新构建一下！”
-
-1.  等大概 **1-2 分钟**。
-2.  刷新你刚才那个“很丑”的网页。
-3.  你应该能看到它变成了一个漂亮的、带有网格和颜色的日历！
-
----
-
-### (备选) 假如刷新后还是没变...
-
-如果过了 5 分钟还没变，请检查一下你的 `tailwind.config.js` 文件。
-
-1.  在 GitHub 点击 `tailwind.config.js` 文件。
-2.  看一眼里面的内容是不是跟我下面的一模一样：
-
-```javascript
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
